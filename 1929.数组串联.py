9b147c41ab37bcfb087dcb2e99657611eaf6a79f
HellOwhatAs <xjq701229@outlook.com>
2023-01-28 19:40:59 +0800

    1929

---
+++ 1929.数组串联.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=1929 lang=python3
+#
+# [1929] 数组串联
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
+# @lc code=end
+
