bd4b18db5e83b86c5d01fbea74a9f0fae3683209
HellOwhatAs <xjq701229@outlook.com>
2023-01-30 10:38:18 +0800

    1379

---
+++ 1379.找出克隆二叉树中的相同节点.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=1379 lang=cpp
+ *
+ * [1379] 找出克隆二叉树中的相同节点
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        if(original == NULL) return NULL;
+        if(target == original) return cloned;
+        TreeNode *ret = getTargetCopy(original -> left, cloned -> left, target);
+        if(ret != NULL) return ret;
+        return getTargetCopy(original -> right, cloned -> right, target);
+    }
+};
+// @lc code=end
+
