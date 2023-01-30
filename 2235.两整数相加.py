e3d69eb03c2da4594cc5d9fd9f0b1e857d500e59
HellOwhatAs <xjq701229@outlook.com>
2023-01-30 17:59:37 +0800

    2235

---
+++ 2235.两整数相加.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=2235 lang=python3
+#
+# [2235] 两整数相加
+#
+
+# @lc code=start
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+# @lc code=end
+
