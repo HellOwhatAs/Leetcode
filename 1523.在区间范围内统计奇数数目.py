455ef3b7c496939cf123a96563e4f14c5e2485ff
HellOwhatAs <xjq701229@outlook.com>
2023-01-31 22:41:15 +0800

    1523

---
+++ 1523.在区间范围内统计奇数数目.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=1523 lang=python3
+#
+# [1523] 在区间范围内统计奇数数目
+#
+
+# @lc code=start
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        return (high - low) // 2 + (low % 2 or high % 2)
+# @lc code=end
+
