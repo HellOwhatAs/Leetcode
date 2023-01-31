3f69fa9f50dc11fc991b476e303f535c4539fdc7
HellOwhatAs <xjq701229@outlook.com>
2023-01-31 21:54:09 +0800

    1450

---
+++ 1450.在既定时间做作业的学生人数.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=1450 lang=python3
+#
+# [1450] 在既定时间做作业的学生人数
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        return sum(s <= queryTime <= e for s, e in zip(startTime, endTime))
+# @lc code=end
+
