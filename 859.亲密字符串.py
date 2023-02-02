39de85efa062fb217a826d68eef2db8d595954ca
HellOwhatAs <xjq701229@outlook.com>
2023-02-02 21:17:33 +0800

    859

---
+++ 859.亲密字符串.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=859 lang=python3
+#
+# [859] 亲密字符串
+#
+
+# @lc code=start
+from collections import Counter
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal): return False
+        diff = [(a, b) for a, b in zip(s, goal) if a != b]
+        n = len(diff)
+        if n > 2 or n == 1: return False
+        if n == 2: return diff[0][0] == diff[1][1] and diff[0][1] == diff[1][0]
+        return max(Counter(s).values()) >= 2
+
+
+# @lc code=end
+
+print(Solution().buddyStrings(s = "abcde", goal = "baced"))
