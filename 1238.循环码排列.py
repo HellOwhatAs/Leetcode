9021a9facea216dc97a2592740f57ef29b5b727d
HellOwhatAs <xjq701229@outlook.com>
2023-02-23 12:03:06 +0800

    1238

--- 1238.循环码排列.py
+++ 1238.循环码排列.py
@@ -10,9 +10,7 @@ class Solution:
     def grayCode(self, n: int) -> List[int]:
         return [0,1] if n == 1 else (tmp := self.grayCode(n - 1)) + [i | (1 << (n - 1)) for i in reversed(tmp)]
     def circularPermutation(self, n: int, start: int) -> List[int]:
-        ret = self.grayCode(n)
-        idx = ret.index(start)
-        return ret[idx:] + ret[:idx]
+        return [i ^ start for i in self.grayCode(n)]
 # @lc code=end
 
 print(Solution().circularPermutation(n = 3, start = 2))
