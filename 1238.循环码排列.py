150514b3dbafae9141d496ac137a8405ad59c18e
HellOwhatAs <xjq701229@outlook.com>
2023-02-23 11:59:50 +0800

    1238

---
+++ 1238.循环码排列.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=1238 lang=python3
+#
+# [1238] 循环码排列
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        return [0,1] if n == 1 else (tmp := self.grayCode(n - 1)) + [i | (1 << (n - 1)) for i in reversed(tmp)]
+    def circularPermutation(self, n: int, start: int) -> List[int]:
+        ret = self.grayCode(n)
+        idx = ret.index(start)
+        return ret[idx:] + ret[:idx]
+# @lc code=end
+
+print(Solution().circularPermutation(n = 3, start = 2))
