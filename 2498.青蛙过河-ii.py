80676075cfd9c323b9ba20ee5efe883e1a2e20c8
HellOwhatAs <xjq701229@outlook.com>
2023-07-12 20:38:18 +0800

    2498

---
+++ 2498.青蛙过河-ii.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=2498 lang=python3
+#
+# [2498] 青蛙过河 II
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    @staticmethod
+    def max_diff(a: List[int]):
+        return max((a[i] - a[i - 1] for i in range(1, len(a))), default=0)
+    def maxJump(self, stones: List[int]) -> int:
+        return max(self.max_diff(stones[::2]), self.max_diff(stones[1::2]), stones[-1] - stones[-2])
+# @lc code=end
+
+print(Solution().maxJump(stones = [0,2,5,6,7]))
+print(Solution().maxJump(stones = [0,3,9]))
