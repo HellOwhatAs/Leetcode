9914d8a57082bf0e25854f3d3ecc9867ade9b62d
HellOwhatAs <xjq701229@outlook.com>
2023-07-12 20:40:49 +0800

    2498

--- 2498.青蛙过河-ii.py
+++ 2498.青蛙过河-ii.py
@@ -11,7 +11,7 @@ class Solution:
     def max_diff(a: List[int]):
         return max((a[i] - a[i - 1] for i in range(1, len(a))), default=0)
     def maxJump(self, stones: List[int]) -> int:
-        return max(self.max_diff(stones[::2]), self.max_diff(stones[1::2]), stones[-1] - stones[-2])
+        return max(self.max_diff(stones[::2]), self.max_diff(stones[1::2]), stones[-1] - stones[-2], stones[1])
 # @lc code=end
 
 print(Solution().maxJump(stones = [0,2,5,6,7]))
