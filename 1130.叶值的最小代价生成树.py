df37271a5f169c375217bc0e33dec34cae00229a
HellOwhatAs <xjq701229@outlook.com>
2023-07-13 08:55:19 +0800

    ??

--- 1130.叶值的最小代价生成树.py
+++ 1130.叶值的最小代价生成树.py
@@ -18,6 +18,19 @@ class Solution:
                 self.max_leaf = self.val
         def __repr__(self) -> str:
             return f"{type(self).__name__}({self.val}, {self.left}, {self.right})"
+        
+        def __graphviz(self, ret: List[str]) -> None:
+            ret.append(f'{id(self)} [label={self.val}];')
+            if self.left is self.right is None: return
+            ret.append(f'{id(self)} -> {id(self.left)};')
+            ret.append(f'{id(self)} -> {id(self.right)};')
+            self.left.__graphviz(ret)
+            self.right.__graphviz(ret)
+            return ret
+
+        def graphviz(self) -> str:
+            return '\n'.join(('digraph{', *self.__graphviz([]), '}'))
+                    
         def sum_not_leaf(self):
             if self.left is self.right is None: return 0
             return self.val + self.left.sum_not_leaf() + self.right.sum_not_leaf()
@@ -32,6 +45,7 @@ class Solution:
                     min_idx = i-1
             right = a.pop(min_idx + 1)
             a[min_idx] = self.Node(min_val, a[min_idx], right)
+        print(a[0].graphviz())
         return a[0].sum_not_leaf()
 # @lc code=end
 
