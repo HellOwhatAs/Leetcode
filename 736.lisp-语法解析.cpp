2064ff19aa3e0aa9c421e1567717b7a300c46a04
HellOwhatAs <xjq701229@outlook.com>
2023-07-14 19:43:56 +0800

    736

--- 736.lisp-语法解析.cpp
+++ 736.lisp-语法解析.cpp
@@ -14,75 +14,88 @@ using namespace std;
 class Solution {
 public:
     unordered_map<string, int> gbl;
-    int find_right(const string& expression, int left) {
-        int pure_left = 1;
-        ++left;
-        while(left < expression.size()){
-            if(expression[left] == '(') ++pure_left;
-            else if(expression[left] == ')') --pure_left;
-            if(pure_left == 0) return left;
-            ++left;
-        }
-        throw "no ')' match";
-    }
-    int literal_find_end(const string& expression, int start) {
+    int token_end(const string& expression, int start) {
         for(int i=start; i<expression.size(); ++i){
             if(expression[i] == ' ' || expression[i] == '(' || expression[i] == ')')return i-1;
         }
         return expression.size()-1;
     }
-    int find_end(const string& expression, int start) {
-        if(expression[start] == '(')return find_right(expression, start);
-        return literal_find_end(expression, start);
-    }
-    int evaluate(const string& expression, int start = 0, int end = -1) {
-        if(end == -1) end = expression.size() - 1;
-        int ret;
-        if(expression[start] == '('){
-            if(expression[start + 1] == 'l'){ // let
-                int i = start + 5;
-                vector<pair<int, int>> elems;
-                while(i < end){
-                    elems.push_back({i, find_end(expression, i)});
-                    i = elems.back().second + 2;
-                }
-                unordered_map<string, int> tmp_gbl;
-                for(int j=0; j<elems.size(); j+=2){
-                    auto [vs, ve] = elems[j];
-                    if(j + 1 < elems.size()){
-                        auto && vname = expression.substr(vs, ve - vs + 1);
-                        tmp_gbl[vname] = gbl[vname];
-                        auto [es, ee] = elems[j+1];
-                        gbl[vname] = evaluate(expression, es, ee);
-                    }
-                    else{
-                        ret = evaluate(expression, vs, ve);
-                    }
-                }
-                for(auto& [k, v]: tmp_gbl){
-                    gbl[k] = v;
-                }
+    struct expr{
+        bool is_leaf;
+        string val;
+        vector<expr*> childs;
+    };
+    int eval(const expr* root) {
+        if(root->is_leaf){
+            if(root->val[0] == '-' || root->val[0] >= '0' && root->val[0] <= '9') return stoi(root->val);
+            return gbl[root->val];
+        }
+        int ret = 0;
+        if(root->val == "let"){
+            unordered_map<string, int> nonlocals;
+            vector<string> locals;
+            for(int i=1; i<root->childs.size(); i+=2){
+                auto & vname = root->childs[i-1]->val;
+                if(gbl.count(vname)) nonlocals[vname] = gbl[vname];
+                else locals.push_back(vname);
+                gbl[vname] = eval(root->childs[i]);
             }
-            else if(expression[start + 1] == 'a'){ // add
-                int i = start + 5, a, b, tmp = find_end(expression, i);
-                a = evaluate(expression, i, tmp);
-                b = evaluate(expression, tmp + 2, find_end(expression, tmp + 2));
-                ret = a + b;
+            ret = eval(root->childs.back());
+            for(auto& [k, v]: nonlocals){
+                gbl[k] = v;
             }
-            else{ // expression[start + 1] == 'm' // mult
-                int i = start + 6, a, b, tmp = find_end(expression, i);
-                a = evaluate(expression, i, tmp);
-                b = evaluate(expression, tmp + 2, find_end(expression, tmp + 2));
-                ret = a * b;
+            for(auto& k: locals) gbl.erase(k);
+        }
+        else if(root->val == "add"){
+            for(auto& i: root->childs){
+                ret += eval(i);
             }
         }
-        else if(expression[start] == '-' || expression[start] >= '0' && expression[start] <= '9'){
-            ret = stoi(expression.substr(start, end - start + 1));
+        else if(root->val == "mult"){
+            ret = 1;
+            for(auto& i: root->childs){
+                ret *= eval(i);
+            }
         }
-        else{
-            ret = gbl[expression.substr(start, end - start + 1)];
-        }
+        else throw root->val + " not implemented";
         return ret;
+    }
+    void free(expr* root) {
+        for(auto& p: root->childs)free(p);
+        delete root;
+    }
+    int evaluate(const string& expression) {
+        stack<expr*> s;
+        int i = 0, tmp;
+        while(i < expression.size()){
+            if(expression[i] == ' ') ++i;
+            else if(expression[i] == '('){
+                s.push(nullptr);
+                ++i;
+            }
+            else if(expression[i] == ')'){
+                expr* p = new expr({false});
+                while(s.top() != nullptr){
+                    p->childs.push_back(s.top());
+                    s.pop();
+                }
+                s.pop();
+                p->val = p->childs.back()->val;
+                delete p->childs.back();
+                p->childs.pop_back();
+                reverse(p->childs.begin(), p->childs.end());
+                s.push(p);
+                ++i;
+            }
+            else{
+                tmp = token_end(expression, i);
+                s.push(new expr({true, expression.substr(i, tmp - i + 1)}));
+                i = tmp + 1;
+            }
+        }
+        tmp = eval(s.top());
+        free(s.top());
+        return tmp;
     }
 };
 // @lc code=end
