b7893c96d62c1465c2a6cfc409335333986faa75
HellOwhatAs <xjq701229@outlook.com>
2023-07-16 21:28:01 +0800

    834

---
+++ 834.树中距离之和.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode.cn id=834 lang=python3
+#
+# [834] 树中距离之和
+#
+
+# @lc code=start
+from typing import List, Optional
+class Solution:
+    def adjlist2tree(self, adjlist: List[List[int]], start: int, father: Optional[int] = None):
+        self.father[start] = father
+        for t in adjlist[start]:
+            if t == father: continue
+            self.tree[start].append(t)
+            self.adjlist2tree(adjlist, t, start)
+    
+    def dfs0(self, start: int):
+        for t in self.tree[start]:
+            self.dfs0(t)
+            self.sz[start] += self.sz[t]
+            self.dp[start] += self.dp[t] + self.sz[t]
+        
+    def swap_root(self, child: int, root: int):
+        '''assuming self.dp and self.sz are initialized with root being root'''
+        __backup__ = self.dp[root], self.dp[child], self.sz[root], self.sz[child]
+        self.dp[root] -= self.dp[child] + self.sz[child]
+        self.sz[root], self.sz[child] = self.sz[root] - self.sz[child], self.sz[root]
+        self.dp[child] += self.dp[root] + self.sz[root]
+        
+        self.ret[child] = self.dp[child]
+        for child1 in self.tree[child]:
+            self.swap_root(child1, child)
+
+        self.dp[root], self.dp[child], self.sz[root], self.sz[child] = __backup__
+
+    def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        self.tree, self.father = [[] for _ in range(n)], [None] * n
+        adjlist = [[] for _ in range(n)]
+        for s, t in edges: adjlist[s].append(t), adjlist[t].append(s)
+        self.adjlist2tree(adjlist, 0)
+        del adjlist
+        
+        self.dp, self.sz, self.ret = [0] * n, [1] * n, [None] * n
+        self.dfs0(0)
+        self.ret[0] = self.dp[0]
+
+        for child in self.tree[0]:
+            self.swap_root(child, 0)
+
+        return self.ret
+
+# @lc code=end
+
+print(Solution().sumOfDistancesInTree(n = 6, edges = [[0,1],[0,2],[2,3],[2,4],[2,5]]))
+print(Solution().sumOfDistancesInTree(n = 1, edges = []))
+print(Solution().sumOfDistancesInTree(n = 2, edges = [[1,0]]))
