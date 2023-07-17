04ec3c907887454da2c2e64527b8e789215a5ef3
HellOwhatAs <xjq701229@outlook.com>
2023-07-17 22:16:48 +0800

    264

--- 264.丑数-ii.cpp
+++ 264.丑数-ii.cpp
@@ -6,7 +6,7 @@
 
 // @lc code=start
 #include<vector>
-#include<iostream>
+#include<tuple>
 using namespace std;
 class Solution {
 public:
@@ -15,11 +15,11 @@ public:
         dp[0] = 1;
         int p2 = 0, p3 = 0, p5 = 0;
         for(int i=1; i<n; ++i){
-            vector<int> candidates = {dp[p2] * 2, dp[p3] * 3, dp[p5] * 5};
-            auto val = dp[i] = min(candidates[0], min(candidates[1], candidates[2]));
-            if(val == candidates[0])++p2;
-            if(val == candidates[1])++p3;
-            if(val == candidates[2])++p5;
+            auto[v2, v3, v5] = make_tuple(dp[p2] * 2, dp[p3] * 3, dp[p5] * 5);
+            auto val = dp[i] = min(v2, min(v3, v5));
+            if(val == v2)++p2;
+            if(val == v3)++p3;
+            if(val == v5)++p5;
         }
         return dp.back();
     }
