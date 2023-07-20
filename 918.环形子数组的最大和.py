4c5cde1603474924d037fcedf3bf3da093e21a52
HellOwhatAs <xjq701229@outlook.com>
2023-07-20 09:08:35 +0800

    918

--- 918.环形子数组的最大和.py
+++ 918.环形子数组的最大和.py
@@ -8,12 +8,15 @@
 from typing import List
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        dp_max, dp_min = [(nums[0], 1)], [(nums[0], 1)]
+        dp_max, dp_min = (nums[0], 1), (nums[0], 1)
+        ret, min_val, min_l = nums[0], nums[0], 1
         for i in range(1, len(nums)):
-            dp_max.append(max((dp_max[-1][0] + nums[i], dp_max[-1][1] + 1), (nums[i], 1)))
-            dp_min.append(min((dp_min[-1][0] + nums[i], dp_min[-1][1] + 1), (nums[i], 1)))
-        ret = max(dp_max)[0]
-        min_val, min_l = min(dp_min)
+            dp_max = max((dp_max[0] + nums[i], dp_max[1] + 1), (nums[i], 1))
+            dp_min = min((dp_min[0] + nums[i], dp_min[1] + 1), (nums[i], 1))
+            
+            ret = max(ret, dp_max[0])
+            if dp_min[0] < min_val: min_val, min_l = dp_min
+
         if min_l < len(nums): ret = max(ret, sum(nums) - min_val)
         return ret
 # @lc code=end
