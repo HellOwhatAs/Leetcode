e2afcb0c4ee01f9000098890f8763ef9eb3f3d3e
HellOwhatAs <xjq701229@outlook.com>
2023-08-13 19:47:25 +0800

    100

---
+++ 100.相同的树.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=100 lang=python3
+#
+# [100] 相同的树
+#
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+# @lc code=start
+from typing import Optional
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is q is None: return True
+        if p is None or q is None: return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+# @lc code=end
+
